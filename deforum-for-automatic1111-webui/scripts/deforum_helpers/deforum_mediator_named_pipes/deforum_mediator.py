eeb79f7536390c19390827ca673e733c678ededa
Rakile <129233989+Rakile@users.noreply.github.com>
2023-08-20 20:14:39 +0200

    Pipe version of deforum mediator 0.7.1

--- deforum-for-automatic1111-webui/scripts/deforum_helpers/deforum_mediator_named_pipes/deforum_mediator.py
+++ deforum-for-automatic1111-webui/scripts/deforum_helpers/deforum_mediator_named_pipes/deforum_mediator.py
@@ -2,25 +2,48 @@ import asyncio
 import websockets
 import pickle
 import time
+import win32pipe, win32file, pywintypes
 import ast
+
 needToUpdateMediator = False
 anim_args_copy = None
 args_copy = None
 root_copy = None
-async def sendAsync(value):
-    async with websockets.connect("ws://localhost:8765") as websocket:
-        # await websocket.send(pickle.dumps(value))
-        try:
-            await asyncio.wait_for(websocket.send(pickle.dumps(value)), timeout=10.0)
-            message = await asyncio.wait_for(websocket.recv(), timeout=10.0)
-        except TimeoutError:
-            print('timeout!')
-        if message.startswith("[\'") and message.endswith("\']"):
-            message = ast.literal_eval(message)
-            if len(message) == 1:
-                message = str(message[0])
+exception_in_a_row = 0
+#async def sendAsync(value):
+#    handle = win32file.CreateFile('\\\\.\\pipe\\Deforum', win32file.GENERIC_READ | win32file.GENERIC_WRITE, 0, None,
+#                                  win32file.OPEN_EXISTING, 0, None)
+#    res = win32pipe.SetNamedPipeHandleState(handle, win32pipe.PIPE_READMODE_MESSAGE, None, None)
+#    bytesToSend = pickle.dumps(value)
+#    win32file.WriteFile(handle, bytesToSend)
+#    message = win32file.ReadFile(handle, 64 * 1024)
+#
+#
+#    win32file.CloseHandle(handle)
+#    return message[1].decode()
 
-        return message
+async def sendAsync(value):
+    bufSize = 64 * 1024
+    handle = win32file.CreateFile('\\\\.\\pipe\\Deforumation', win32file.GENERIC_READ | win32file.GENERIC_WRITE, 0, None,
+                                  win32file.OPEN_EXISTING, 0, None)
+    res = win32pipe.SetNamedPipeHandleState(handle, win32pipe.PIPE_READMODE_MESSAGE, None, None)
+    bytesToSend = pickle.dumps(value)
+    win32file.WriteFile(handle, bytesToSend)
+    result, data = win32file.ReadFile(handle, bufSize)
+    message = data
+    while len(data) == bufSize:
+        print("More data has to be read (normal pipe async):"+ str(len(data)))
+        result, data = win32file.ReadFile(handle, bufSize)
+        message += data
+
+    win32file.CloseHandle(handle)
+
+    message = message.decode()
+    if message.startswith("[\'") and message.endswith("\']"):
+        message = ast.literal_eval(message)
+        if len(message) == 1:
+            message = str(message[0])
+    return message
 
 def mediator_set_anim_args(anim_args, args, root):
     global anim_args_copy
@@ -46,8 +69,10 @@ def updateMediator(): #No validation is made that  the websocket server (Mediato
 
 def mediator_getValue(param):
     global needToUpdateMediator
+    global exception_in_a_row
     checkerrorConnecting = True
     needToUpdateMediator = False
+    exception_in_a_row = 0
     while checkerrorConnecting:
         try:
             return_value = asyncio.run(sendAsync([0, param, 0]))
@@ -56,16 +81,20 @@ def mediator_getValue(param):
                 updateMediator()
             return return_value
         except Exception as e:
-            print("Deforum Mediator Error:" + str(e))
-            print("...while trying to get parameter ("+str(param)+")")
-            print("The Deforumation Mediator, is probably not connected (waiting 5 seconds, before trying to reconnect...)")
-            time.sleep(5)
-            needToUpdateMediator = True
+            exception_in_a_row += 1
+            if exception_in_a_row > 3:
+                print("Deforum Mediator Error:" + str(e))
+                print("...while trying to send parameter ("+str(param)+")")
+                print("The Deforumation Mediator, is probably not connected (waiting 0.2 seconds, before trying to reconnect...)")
+                needToUpdateMediator = True
+            time.sleep(0.05)
 
 def mediator_setValue(param, value):
     global needToUpdateMediator
+    global exception_in_a_row
     checkerrorConnecting = True
     needToUpdateMediator = False
+    exception_in_a_row = 0
     while checkerrorConnecting:
         try:
             return_value = asyncio.run(sendAsync([1, param, value]))
@@ -74,8 +103,10 @@ def mediator_setValue(param, value):
                 updateMediator()
             return return_value
         except Exception as e:
-            print("Deforum Mediator Error:" + str(e))
-            print("...while trying to send parameter ("+str(param)+") with value("+str(value)+")")
-            print("The Deforumation Mediator, is probably not connected (waiting 5 seconds, before trying to reconnect...)")
-            time.sleep(5)
-            needToUpdateMediator = True
+            exception_in_a_row += 1
+            if exception_in_a_row > 3:
+                print("Deforum Mediator Error:" + str(e))
+                print("...while trying to send parameter ("+str(param)+") with value("+str(value)+")")
+                print("The Deforumation Mediator, is probably not connected (waiting 0.2 seconds, before trying to reconnect...)")
+                needToUpdateMediator = True
+            time.sleep(0.05)
